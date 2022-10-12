809e34ce17aaac5286daec1ee0326c87ed486124
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-10-12 10:22:45 +0700

    update database

--- sql/Student-Management.sql
+++ sql/Student-Management.sql
@@ -29,13 +29,13 @@ INSERT INTO Classroom(class_name) VALUES ('C0522G1');
 INSERT INTO Classroom(class_name) VALUES ('C0422H1');
 
 -- Class
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Dang Duy Hung', '1998-10-01', 'Ha Noi', '0983672224', 'ddh@gcodegym.vn', 1);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Ta Phuong Linh', '1993-10-22', 'Ha Nam', '0785367724', 'tpl@codegym.vn', 1);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Do Phuong Nam', '1997-06-12', 'Bac Giang', '0983682028', 'dpn@codegym.vn', 2);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyen Duy Khanh', '1987-04-26', 'Ha Noi', '0930284421', 'ndk@codegym.vn', 3);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Vu Hien Luong', '1997-12-10', 'Bac Ninh', '0915814526', 'vhl@codegym.vn', 4);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyen Duy Phong', '1999-05-16', 'Quang Ninh', '0369678405', 'ndp@codegym.vn', 4);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyen Duy Hung', '1998-03-15', 'Ha Noi', '0393827070', 'ndh@codegym.vn', 4);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Hoang Phuong Nam', '1992-06-12', 'Hung Yen', '0319271673', 'hpn@codegym.vn', 5);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyen The An', '1996-09-10', 'Thai Nguyen', '0816372204', 'nta@codegym.vn', 6);
-INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Vu Hai Dang', '1996-09-10', 'Hai Phong', '0348274662', 'vhd@codegym.vn', 6);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Đặng Duy Hùng', '1998-10-01', 'Hà Nội', '0983672224', 'ddh@gcodegym.vn', 1);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Tạ Phương Linh', '1993-10-22', 'Hà Nam', '0785367724', 'tpl@codegym.vn', 1);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Đỗ Phương Nam', '1997-06-12', 'Bắc Giang', '0983682028', 'dpn@codegym.vn', 2);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyễn Duy Khánh', '1987-04-26', 'Hà Nội', '0930284421', 'ndk@codegym.vn', 3);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Vũ Hiền Lương', '1997-12-10', 'Bắc Ninh', '0915814526', 'vhl@codegym.vn', 4);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyễn Duy Phong', '1999-05-16', 'Quảng Ninh', '0369678405', 'ndp@codegym.vn', 4);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyễn Duy Hùng', '1998-03-15', 'Hà Nội', '0393827070', 'ndh@codegym.vn', 4);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Hoàng Phương Nam', '1992-06-12', 'Hưng Yên', '0319271673', 'hpn@codegym.vn', 5);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Nguyễn Thế An', '1996-09-10', 'Thái Nguyên', '0816372204', 'nta@codegym.vn', 6);
+INSERT INTO Student(name, date_of_birth, address, phone, email, class_id) VALUES ('Vũ Hải Đăng', '1996-09-10', 'Hải Phòng', '0348274662', 'vhd@codegym.vn', 6);
